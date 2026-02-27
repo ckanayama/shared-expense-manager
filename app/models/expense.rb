d7846fe3000ec317b17d31e60df651a68ada2307
ckanayama <knym.777@gmail.com>
2026-02-27 23:24:42 +0900

    バリデーション追加

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,6 @@
 class Expense < ApplicationRecord
+  validates :occurred_on, presence: true
+  validates :payer, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  validates :category, presence: true
 end
---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe Expense, type: :model do
+  describe 'amount_positive' do
+    let(:expense) { Expense.new(occurred_on: Time.zone.now, payer: 1, amount:, category: 1) }
+
+    context 'amount is positive' do
+      let(:amount) { 100 }
+
+      it 'be valid' do
+        expect(expense).to be_valid
+      end
+    end
+
+    context 'amount is positive' do
+      let(:amount) { 0 }
+
+      it 'be valid' do
+        expect(expense).to be_invalid
+      end
+    end
+
+    context 'amount is negative' do
+      let(:amount) { -100 }
+
+      it 'not be valid' do
+        expect(expense).to be_invalid
+      end
+    end
+  end
+end
