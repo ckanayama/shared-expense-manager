273b28767dc611c89ee85a4a09221b84feb4b5a1
ckanayama <knym.777@gmail.com>
2026-02-27 22:45:47 +0900

    expenses テーブル追加

--- db/migrate/20260224133718_create_expenses.rb
+++ db/migrate/20260224133718_create_expenses.rb
@@ -1,7 +1,15 @@
 class CreateExpenses < ActiveRecord::Migration[8.0]
   def change
     create_table :expenses do |t|
+      t.date :occurred_on, null: false, index: true
+      t.integer :payer, null: false
+      t.integer :amount, null: false
+      t.integer :category, null: false, index: true
+      t.string :memo
+
       t.timestamps
     end
+
+    add_check_constraint :expenses, "amount > 0", name: "amount_positive"
   end
 end
