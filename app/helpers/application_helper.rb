583b6f2c43639882bcae4fa56f6b9ab188e59a8c
ckanayama <knym.777@gmail.com>
2026-03-08 14:20:18 +0900

    グローバルナビゲーションを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def latest_statement_month
+    Statement.order(date: :desc).pick(:date) || Date.current
+  end
+
   def random_character_image
     images = Dir.glob(Rails.public_path.join("images/characters/*"))
     return nil if images.empty?
